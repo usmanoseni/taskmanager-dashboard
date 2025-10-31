545f35e2fa37c263bd65e55f98ebd1b747ff3fde
usmanoseni <oseniusmanrotimi@gmail.com>
2025-10-31 13:14:35 +0100

    updatated version

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  plugins: [react(), tailwindcss()],
   base: '/taskmanager-dashboard/', // ✅ your GitHub repo name here
 })
