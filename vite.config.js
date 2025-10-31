e039771c1b6e4d9c7ee9e6d247b8cb0cee9d7bd0
usmanoseni <oseniusmanrotimi@gmail.com>
2025-10-31 13:08:41 +0100

    updatated version

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react'
-import path from 'path';
-import tailwindcss from '@tailwindcss/vite';
 
-
-// https://vite.dev/config/
 export default defineConfig({
-    resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  plugins: [react(), tailwindcss()],
-    base: '/react-js-jsx-and-css-mastering-front-end-development-usmanoseni/',
+  plugins: [react()],
+  base: '/taskmanager-dashboard/', // ✅ your GitHub repo name here
 })
